1799464861a7b18dec5bc958363ff100ac4196e0
shlayb <ayubisal1345@gmail.com>
2024-11-11 20:33:18 +0700

    sudah bisa print ke modul

--- dump.cpp
+++ dump.cpp
@@ -1,23 +1,25 @@
 #include <Wire.h>
 #include <Arduino.h>
+
 const int MPU = 0x68; // MPU6050 I2C address
 int16_t tempAccX, tempAccY, tempAccZ;
 float AccX, AccY, AccZ;
 int16_t tempGyroX, tempGyroY, tempGyroZ;
 float GyroX, GyroY, GyroZ;
-float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ;
+float accAngleX, accAngleY, gyroAngleX, gyroAngleY;
 float roll, pitch, yaw;
 float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
-unsigned long elapsedTime, currentTime, previousTime;
-const int numCalibrationSamples = 500;
-const float alpha = 0.95; // Complementary filter constant
+unsigned long previousTime;
+const int numCalibrationSamples = 2000;
+const float alpha = 0.9; // Complementary filter constant
 
-const float Q = 1;
-const float R = 1;
+// Kalman filter parameters
+const float Q = 0.01; // process noise covariance
+const float R = 0.03;  // measurement noise covariance
+float P_est_X = 1, P_est_Y = 1, P_est_Z = 1;  // Separate estimates per axis
 float GyroX_est, GyroY_est, GyroZ_est;
-float PX_est, PY_est, PZ_est;
 
-void readAcc(){
+void readAcc() {
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);
   Wire.endTransmission(false);
@@ -32,9 +34,9 @@ void readAcc(){
 
   accAngleX = (atan2(AccY, sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorX;
   accAngleY = (atan2(-AccX, sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) + AccErrorY;
-};
+}
 
-void readGyro(){
+void readGyro() {
   Wire.beginTransmission(MPU);
   Wire.write(0x43);
   Wire.endTransmission(false);
@@ -43,31 +45,32 @@ void readGyro(){
   tempGyroY = (Wire.read() << 8 | Wire.read());
   tempGyroZ = (Wire.read() << 8 | Wire.read());
 
-  GyroX = ((float)tempGyroX) / 131.0;
-  GyroY = ((float)tempGyroY) / 131.0;
-  GyroZ = ((float)tempGyroZ) / 131.0;
+  GyroX = ((float)tempGyroX) / 131.0 - GyroErrorX;
+  GyroY = ((float)tempGyroY) / 131.0 - GyroErrorY;
+  GyroZ = ((float)tempGyroZ) / 131.0 - GyroErrorZ;
 
-  // add the gyro error to the gyro values
-  GyroX = GyroX - GyroErrorX;
-  GyroY = GyroY - GyroErrorY;
-  GyroZ = GyroZ - GyroErrorZ;
+  // Kalman filter per sumbu
+  P_est_X += Q;
+  float K_X = P_est_X / (P_est_X + R);
+  GyroX_est = GyroX_est + K_X * (GyroX - GyroX_est);
+  P_est_X = (1 - K_X) * P_est_X;
 
-  // kalman filter
-  P_est += Q; // Prediksi error dari estimasi
+  P_est_Y += Q;
+  float K_Y = P_est_Y / (P_est_Y + R);
+  GyroY_est = GyroY_est + K_Y * (GyroY - GyroY_est);
+  P_est_Y = (1 - K_Y) * P_est_Y;
 
-  // Update
-  float K = P_est / (P_est + R);
-  GyroX_est = GyroX_est + K * (GyroX - GyroX_est);
-  GyroY_est = GyroY_est + K * (GyroY - GyroY_est);
-  GyroZ_est = GyroZ_est + K * (GyroZ - GyroZ_est);
-  P_est = (1 - K) * P_est;
+  P_est_Z += Q;
+  float K_Z = P_est_Z / (P_est_Z + R);
+  GyroZ_est = GyroZ_est + K_Z * (GyroZ - GyroZ_est);
+  P_est_Z = (1 - K_Z) * P_est_Z;
 
   GyroX = GyroX_est;
   GyroY = GyroY_est;
   GyroZ = GyroZ_est;
-};
+}
 
-void setup(){
+void setup() {
   Serial.begin(19200);
   Wire.begin();
   Wire.beginTransmission(MPU);
@@ -75,8 +78,7 @@ void setup(){
   Wire.write(0x00);
   Wire.endTransmission(true);
 
-  for(int i = 0; i < numCalibrationSamples; i++){
-    // Calculate gyro error
+  for (int i = 0; i < numCalibrationSamples; i++) {
     Wire.beginTransmission(MPU);
     Wire.write(0x43);
     Wire.endTransmission(false);
@@ -85,74 +87,76 @@ void setup(){
     tempGyroY = (Wire.read() << 8 | Wire.read());
     tempGyroZ = (Wire.read() << 8 | Wire.read());
 
-    GyroErrorX = GyroErrorX + ((float)tempGyroX / 131.0);
-    GyroErrorY = GyroErrorY + ((float)tempGyroY / 131.0);
-    GyroErrorZ = GyroErrorZ + ((float)tempGyroZ / 131.0);
+    GyroErrorX += ((float)tempGyroX / 131.0);
+    GyroErrorY += ((float)tempGyroY / 131.0);
+    GyroErrorZ += ((float)tempGyroZ / 131.0);
   }
-  GyroErrorX = GyroErrorX / numCalibrationSamples;
-  GyroErrorY = GyroErrorY / numCalibrationSamples;
-  GyroErrorZ = GyroErrorZ / numCalibrationSamples;
+  GyroErrorX /= numCalibrationSamples;
+  GyroErrorY /= numCalibrationSamples;
+  GyroErrorZ /= numCalibrationSamples;
 
-  tempGyroX = (Wire.read() << 8 | Wire.read());
-  tempGyroY = (Wire.read() << 8 | Wire.read());
-  tempGyroZ = (Wire.read() << 8 | Wire.read());
+  // Initial values
+  GyroX_est = GyroErrorX;
+  GyroY_est = GyroErrorY;
+  GyroZ_est = GyroErrorZ;
 
-  GyroX_est = ((float)tempGyroX) / 131.0;
-  GyroY_est = ((float)tempGyroY) / 131.0;
-  GyroZ_est = ((float)tempGyroZ) / 131.0;
+  roll = 0;
+  pitch = 0;
+  yaw = 0;
 
-  Serial.print("Gyro Error X: ");
-  Serial.print(GyroErrorX);
-  Serial.print("\t");
-  Serial.print("Gyro Error Y: ");
-  Serial.print(GyroErrorY);
-  Serial.print("\t");
-  Serial.print("Gyro Error Z: ");
-  Serial.print(GyroErrorZ);
-  Serial.println("\t");
-  delay(20);
+  previousTime = millis();
+
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
 }
 
-void loop(){
-  // Calculate Milliseconds
+void loop() {
+  unsigned long currentTime = millis();
+  float elapsedTime = (currentTime - previousTime) / 1000.0;
   previousTime = currentTime;
-  currentTime = millis();
-  elapsedTime = (currentTime - previousTime);
 
-  // Read Accelerometer Data
   readAcc();
-
-  // read Gyro
   readGyro();
 
-  // Calculate the angle
-  gyroAngleX = gyroAngleX + GyroX * (elapsedTime / 1000.0);
-  gyroAngleY = gyroAngleY + GyroY * (elapsedTime / 1000.0);
-  yaw = yaw + GyroZ * (elapsedTime / 1000.0);
-  
-  // Complementary Filter for Roll and Pitch
-  roll = alpha * (roll + GyroX * (elapsedTime / 1000.0)) + (1 - alpha) * accAngleX;
-  pitch = alpha * (pitch + GyroY * (elapsedTime / 1000.0)) + (1 - alpha) * accAngleY;
-  yaw = (yaw > 180 ? fmod(yaw, 180.0) : yaw);
+  // Integrasi gyro untuk menghitung angle
+  gyroAngleX += GyroX * elapsedTime;
+  gyroAngleY += GyroY * elapsedTime;
+  yaw += GyroZ * elapsedTime;
 
-  // Print the values on the serial monitor
-  Serial.print("Roll: ");
-  Serial.print(roll);
-  Serial.print("\t");
-  Serial.print("Pitch: ");
-  Serial.print(pitch);
-  Serial.print("\t");
-  Serial.print("Yaw: ");
-  Serial.print(yaw);
-  Serial.print("\t");
-  Serial.print("GyroX: ");
-  Serial.print(GyroX);
-  Serial.print("\t");
-  Serial.print("GyroY: ");
-  Serial.print(GyroY);
-  Serial.print("\t");
-  Serial.print("GyroZ: ");
-  Serial.print(GyroZ);
-  Serial.println("\t");
+  // Complementary Filter untuk Roll dan Pitch
+  roll = alpha * (roll + GyroX * elapsedTime) + (1 - alpha) * accAngleX;
+  pitch = alpha * (pitch + GyroY * elapsedTime) + (1 - alpha) * accAngleY;
+
+  // Penanganan overflow untuk yaw agar tetap dalam rentang [-180, 180]
+  if (yaw > 180) yaw -= 360;
+  else if (yaw < -180) yaw += 360;
+
+  // // Print hasil
+  // Serial.print("Roll: ");
+  // Serial.print(roll);
+  // Serial.print("\tPitch: ");
+  // Serial.print(pitch);
+  // Serial.print("\tYaw: ");
+  // Serial.print(yaw);
+  // Serial.print("\tGyroX: ");
+  // Serial.print(GyroX);
+  // Serial.print("\tGyroY: ");
+  // Serial.print(GyroY);
+  // Serial.print("\tGyroZ: ");
+  // Serial.println(GyroZ);
+
+  if(Serial2.available()) {
+    char c = Serial2.read();
+    if(c == 'r') {
+      if(roll > 40){
+        String dataToSend = "Roll: " + String(roll) + "\tPitch: " + String(pitch) + "\tYaw: " + String(yaw) +"\n";
+        Serial2.print(dataToSend);
+        
+      }else{
+        String dataToSend = "WOYYYY MAU JATUHHH";
+        Serial2.print(dataToSend);
+      }
+    }
+  }
+
   delay(20);
 }
